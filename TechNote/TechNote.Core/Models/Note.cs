e5a20069ffc11106f0af7a9f541f74e30e101f54
yuqima17 <yuqi_m@yahoo.com>
2019-06-02 16:57:51 -0400

    created note class and related repository in the memry cache

---
+++ TechNote/TechNote.Core/Models/Note.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechNote.Core
+{
+    public class Note
+    {
+
+        public string Id { get; set; }
+
+       
+        public string dateCreated { get; set; }
+        public string dateModified { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [StringLength(300)]
+        public string Description { get; set; }
+
+        [StringLength(2000)]
+        public string NoteContent { get; set; }
+        //code samples () sample:string of code
+        public Note()
+        {
+            Id = Guid.NewGuid().ToString();
+            dateCreated= DateTime.Today.ToString("dd-MM-yyyy");
+        }
+    }
+}
